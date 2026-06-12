0b6219fb5f2b3c286ed8741fb81205c008a967b2
sreekanth <valmikisrikanth89@gmail.com>
2026-06-12 19:53:48 +0530

    removed unwanted content

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -74,14 +74,14 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Lovable App" },
-      { name: "description", content: "Lovable Generated Project" },
-      { name: "author", content: "Lovable" },
-      { property: "og:title", content: "Lovable App" },
-      { property: "og:description", content: "Lovable Generated Project" },
+      { title: "Brochure Generator App" },
+      { name: "description", content: "Sreekanth" },
+      { name: "author", content: "Sreekanth" },
+      { property: "og:title", content: "Brochure App" },
+      { property: "og:description", content: "Generates the Brochure Based on the Company " },
       { property: "og:type", content: "website" },
       { name: "twitter:card", content: "summary" },
-      { name: "twitter:site", content: "@Lovable" },
+      { name: "twitter:site", content: "@Brochure" },
     ],
     links: [
       {
